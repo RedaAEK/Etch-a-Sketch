6fdea486ca68e3ff1d8d61081a9f3d840cb15fb7
RedaAEK <redabelhiaofficial@gmail.com>
2025-11-03 10:35:27 +0100

    updated Etch-a-Sketch app with animations and bug fixes

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 const container = document.querySelector(".container");
 const changeGrid = document.getElementsByTagName("button")[0];
 const resetGrid = document.getElementsByTagName("button")[1];
-let dimensions;
-console.log(containerWidth)
-function generateGrid(Squares) {
-  if (Squares <= 100){
-  dimensions=Number(containerWidth/Squares)
-  for (let i = 0; i < (Squares*Squares); i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.className = "square";
-    container.appendChild(gridSquare);
-    gridSquare.style.boxSizing="border-box"
-    gridSquare.style.border = "1px solid black";
-    gridSquare.style.width = `${dimensions}px`;
-    gridSquare.style.height = `${dimensions}px`;
-    gridSquare.addEventListener("mouseover", () => {
-      let r = Math.floor(Math.random() * 256);
-      let g = Math.floor(Math.random() * 256);
-      let b = Math.floor(Math.random() * 256);
-      gridSquare.style.background = `rgb(${r},${g},${b})`;
-    });
-  }} else {
-    alert("You've reached the limit which is 100 per row")
+// random colors :
+const getRandomColor = (element) => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+   return `rgb(${r},${g},${b})`;
+};
+// generate grids 
+function generateGrid(gridSize) {
+container.replaceChildren();
+  if (gridSize <= 100) {
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.className = "square";
+      container.appendChild(gridSquare);
+      gridSquare.addEventListener("mouseover", () => {
+       gridSquare.style.background = getRandomColor();
+      });
+    }
+  } else if (isNaN(gridSize) || !gridSize) {
+    alert("That's not a number , please enter a number");
+  } else {
+    alert("You've reached the limit which is 100 per row");
   }
 }
-generateGrid(16)
-// }
+generateGrid(16);
+// changesize event 
 changeGrid.addEventListener("click", () => {
-  container.innerHTML=''
-  gridPrompt = prompt(`How many grid you want per side)`);
-generateGrid(gridPrompt)
+  const gridPrompt = prompt(`How many grid you want per side)`);
+
+  generateGrid(parseInt(gridPrompt));
 });
+// Reset Grid event
 resetGrid.addEventListener("click", () => {
-  container.innerHTML = "";
-  generateGrid(16)
+  generateGrid(16);
 });
